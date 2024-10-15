6b4223ebe871bf7c848a2706761c847df2b31deb
paulErdos <vincesteffens@gmail.com>
2024-10-15 03:40:52 -0700

    Oriented with list component and basic direction

--- nextui/experiment_0-pages/app-name/pages/pricing/index.tsx
+++ nextui/experiment_0-pages/app-name/pages/pricing/index.tsx
@@ -7,6 +7,8 @@ export default function DocsPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Pricing</h1>
+          <div>I intend to put this in a card: "https://react-select.com/home"</div>
+          <div>Note: if recipes can be saved, named, and used as ingredients, meals of recipe servings are trivial</div>
         </div>
       </section>
     </DefaultLayout>
