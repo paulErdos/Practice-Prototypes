2d816fee84eb67c38f34f72860f49041c718a18f
vincent <vincesteffens@gmail.com>
2025-04-05 22:02:23 -0700

    Don't start if already running.

--- Starters/Backend/Servers/poetry-uvicorn-falcon/server-run.sh
+++ Starters/Backend/Servers/poetry-uvicorn-falcon/server-run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+# Already running?
+if [ $(./server-status.sh) -eq 1 ]; then
+    echo 'Running.'
+    exit
+fi
+
+
 # Start Server
 cd asgi-starter/ && poetry run ./start-server.sh &
 cd -
