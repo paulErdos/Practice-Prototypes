f29cfc8400ed9d9b079e5ea30548eff1c5c14dd8
NICK BUENO <tehbobum@gmail.com>
2025-06-10 03:08:59 -0700

    script up install of some dependencies

---
+++ Prototypes/nutritionFacts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#brew install pipx
+#pipx ensurepath
+#pipx install poetry
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.16.0".
+nvm current # Should print "v22.16.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
