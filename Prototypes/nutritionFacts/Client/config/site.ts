78bdb73ea145e3b25a1bd0bd2a90923c141b801f
vincent <vincesteffens@gmail.com>
2025-06-27 08:18:55 -0700

    Only display certain navbar items

--- Prototypes/nutritionFacts/Client/config/site.ts
+++ Prototypes/nutritionFacts/Client/config/site.ts
@@ -30,10 +30,12 @@ export const siteConfig = {
       label: "Recipes",
       href: "/Recipes",
     },
+    /*
     {
       label: "Experiments",
       href: "/Experiments-Warehouse",
     },
+    */
   ],
   navMenuItems: [
     {
