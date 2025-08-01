9a4c86c46340822d543628837ef1a60811323047
vincent <vincesteffens@gmail.com>
2025-08-01 10:14:53 -0700

    Bash arithmetic notes

---
+++ Practice/languages/bash/TODO.sh
@@ -0,0 +1,3 @@
+# TODO
+# Use arithmetic operator to iterative compute golden ratio
+# ooh also continued fraction version maybe
---
+++ Practice/languages/bash/arithmetic.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Use double parens
+
+
+echo $((7 * 7))
+echo $((144 + 233))
