e79fd8888d0e8bdf944628a5d51a54b0e5dc16fd
paulErdos <vincesteffens@gmail.com>
2025-09-13 02:20:38 -0700

    Generify whereami

--- Starters/multicast/resource_advertisement_exploration/where-am-i.sh
+++ Starters/multicast/resource_advertisement_exploration/where-am-i.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-h=$(hostname -I 2>&1 > /dev/null)
-[ ! -z $? ] && my_ip=$(echo $h | awk '{ print $1 }')
-
-# Otherwise try
-my_ip=$(ifconfig | awk '/inet / && !/127.0.0.1/ {print $2}')
+if which ip >/dev/null 2>&1; then
+    my_ip=$(ip route get 1.1.1.1 | awk '{print $7; exit}' 2>/dev/null)
+else
+    my_ip=$(ifconfig | awk '/inet / && !/127.0.0.1/ {print $2}' | head -1)
+fi
 
 echo $my_ip
